d652e4b28e77a2bf35515cfd229cbc1cb5aeba11
mirmil <15006763+Mirmilcass@users.noreply.github.com>
2024-10-11 17:49:41 +0900

    하위 부품 정상 표기 | slot id를 사용한 미장착 상태 전파 조치

--- src/components/build/SlotSelect.vue
+++ src/components/build/SlotSelect.vue
@@ -1,6 +1,6 @@
 <script setup>
 import _ from "lodash";
-import {computed, onUpdated} from "vue";
+import {computed} from "vue";
 
 const props = defineProps({
   item          : Object,
@@ -13,10 +13,6 @@ const props = defineProps({
       value : []
     }
   }
-})
-
-onUpdated(() => {
-  props.containsItems.value = props.item.containsItems.flatMap(v => v.item.id)
 })
 
 const containsItems = computed(() => {
@@ -50,16 +46,18 @@ function inChild(item) {
   return Object.keys(item).includes("properties") && Object.keys(item.properties).includes('slots') && item.properties.slots.length > 0;
 }
 
-function selValue(items, isVal) {
-  const find = items.find(v => containsItems.value.includes(v.id));
+function selValue(slot, isVal) {
+  const find = slot.filters.allowedItems.find(v => containsItems.value.includes(v.id));
   if (!find) return 'null';
   return isVal ? find.normalizedName : find;
 }
 
 function updateContainsItems(oldItem, normalName) {
   let filter = containsItems.value.filter(v => v !== oldItem.id)
-  const find = props.mods.find(v => v.normalizedName === normalName)
-  filter.push(find.id)
+  if (normalName !== "null") {
+    const find = props.mods.find(v => v.normalizedName === normalName)
+    filter.push(find.id)
+  }
   props.containsItems.value = filter
 }
 
@@ -67,15 +65,24 @@ function updateContainsItems(oldItem, normalName) {
 
 <template>
   <div v-for="(slot, index) in getSlots()" :key="index" :style="left">
-    <span>{{ slot.name }}</span>
-    <select
-        :value="selValue(slot.filters.allowedItems, true)"
-        @change="event => updateContainsItems(selValue(slot.filters.allowedItems), event.target.value)"
-    >
-      <option :key="0" :value="'null'">미장착</option>
-      <option v-for="(part, index) in slot.filters.allowedItems" :key="index+1" :value="part.normalizedName">{{ part.name }}</option>
-    </select>
-    <SlotSelect v-if="inChild(selValue(slot.filters.allowedItems))" :item="selValue(slot.filters.allowedItems)" :mods="props.mods" :depth="props.depth + 1" :containsItems="props.containsItems"/>
+    <div style="display: flex; flex-direction: column;">
+      <span style="margin: 2px 0">{{ slot.name }}</span>
+      <select
+          :value="selValue(slot, true)"
+          @change="event => updateContainsItems(selValue(slot), event.target.value)"
+          style="margin-left: 5px; color: var(--color-text); background-color: var(--color-background-soft)"
+      >
+        <option :key="0" :value="'null'">미장착</option>
+        <option v-for="(part, index) in slot.filters.allowedItems" :key="index+1" :value="part.normalizedName">{{ part.name }}</option>
+      </select>
+    </div>
+    <SlotSelect
+        v-if="inChild(selValue(slot))"
+        :item="selValue(slot)"
+        :mods="props.mods"
+        :depth="props.depth + 1"
+        :containsItems="{value: containsItems}"
+    />
   </div>
 </template>
 
